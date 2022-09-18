f46fbd96bf4f3095026708005d95661b129e6c10
Martín <desutroying2000@gmail.com>
2022-09-17 22:28:08 -0300

    BaSP-W05-2022:Solved exercise 5 "for" part c and d

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -6,6 +6,10 @@
     // c. Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto
     // a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable
     // sentence. Al final mostrar una única alerta con la cadena completa.
+    // d. Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número
+    // de la repetición, es decir que al final de la ejecución del bucle for debería haber 10
+    // elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola
+    // del navegador el array final (utilizar console.log)
 
 // Parte a
 
@@ -15,9 +19,29 @@ for (var i = 0; i < arrayOfWords.length; i++) {
     alert(arrayOfWords[i]); 
 }   //Recorre el array y muestra sus valores
 
-//Parte b
+//  Parte b
 
 for (var i = 0; i < arrayOfWords.length; i++) {
     arrayOfWords[i] = arrayOfWords[i].substring(0,1).toUpperCase() + arrayOfWords[i].substring(1).toLowerCase();
     alert (arrayOfWords[i]);
 }
+
+//  Parte c
+
+var sentence = '';
+
+for (var i = 0; i < arrayOfWords.length; i++) {
+    sentence = sentence + arrayOfWords[i];
+}
+
+alert(sentence);
+
+// Parte d
+
+var newArray = [];
+
+for (var i = 0; i < 10; i++) {
+    newArray[i] = i;
+}
+
+console.log(newArray);
